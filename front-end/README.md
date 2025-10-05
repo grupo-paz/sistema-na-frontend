69526d71403b5493cb40d335b6c95daa7915249a
Ryan Pavini <r236799@dac.unicamp.br>
2025-10-05 13:34:37 -0300

    fix(front): Corrige rotas do backend e alinha com escopo da Sprint 1

---
+++ front-end/README.md
@@ -0,0 +1,12 @@
+# Front end
+
+Para rodar esse projeto:
+
+
+configure o arquivo .env com as configuracoes necessárias baseado no arquivo .env.exemple
+
+nvm use (utilizar o node atual)
+
+npm i
+
+npm run dev
---
+++ front-end/src/App.tsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { LoginPage } from "./pages/Login";
+import { AdminDashboard } from "./pages/AdminDashboard";
+import { DefinePasswordPage } from "./pages/definePasswordPage";
+import { ProtectedRoute } from "./routes/ProtectedRoute";
+import { RootRedirect } from "./routes/RootRedirect";
+import "./App.css";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<RootRedirect />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/define-password" element={<DefinePasswordPage />} />
+        <Route element={<ProtectedRoute />}>
+          <Route path="/admin" element={<AdminDashboard />} />
+        </Route>
+        <Route path="*" element={<RootRedirect />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
