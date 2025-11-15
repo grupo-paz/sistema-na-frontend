bb2de33dce557dc6f2ac337c642233d44d8d8377
Ryan Pavini <r236799@dac.unicamp.br>
2025-11-14 22:56:17 -0300

    fix(security): Finaliza limpeza de token expirado no ProtectedRoute

--- src/routes/protected-route.tsx
+++ src/routes/protected-route.tsx
@@ -1,12 +1,18 @@
 import { Navigate, Outlet } from "react-router-dom"
-import { authStorage, isTokenValid } from "../services"
+import { authStorage, isTokenValid } from "../services" 
 
 export function ProtectedRoute() {
     const access = authStorage.getAccessToken()
 
-    if (!access || !isTokenValid(access)) {
+    if (!access) {
         return <Navigate to="/login" replace />
     }
     
+    if (!isTokenValid(access)) {
+
+        authStorage.clearTokens(); 
+        
+        return <Navigate to="/login" replace />
+    }
     return <Outlet />
 }
