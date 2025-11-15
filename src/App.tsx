7c8bfb44bb63add5ab3ed5fd02f0f835b4189ed2
Ryan Pavini <r236799@dac.unicamp.br>
2025-11-14 22:08:35 -0300

    revert: Reverte tentativa de roteamento por dominio para estabilizar build

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,18 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { ProtectedRoute } from "./routes"; 
-import { AdminAdministrators, Admin, AdminProfile, AdminMeetings, DefinePassword, Home, LoginPage, AdminChangePassword, AdminSecretary, ForgotPassword, SecretaryPage, EventsPage, MeetingsPage } from "./pages";
-
-const DomainChecker = () => {
-    const hostname = window.location.hostname;
-
-    if (hostname.includes('admin')) {
-        return <Navigate to="/login" replace />;
-    }
-    return <Home />;
-};
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ProtectedRoute, RootRedirect } from "./routes";
+import { AdminAdministrators, Admin, AdminProfile, AdminMeetings, DefinePassword, Home, LoginPage, AdminChangePassword, AdminSecretary, ForgotPassword, AdminEvents, SecretaryPage, EventsPage, MeetingsPage } from "./pages";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {}
-        <Route path="/" element={<DomainChecker />} />
-        
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/define-password" element={<DefinePassword />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/secretaria" element={<SecretaryPage />} />
         <Route path="/eventos" element={<EventsPage />} />
         <Route path="/reunioes" element={<MeetingsPage />} />
-        
-        {}
         <Route element={<ProtectedRoute />}>
           <Route path="/admin/*" element={<Admin />} />
           <Route path="/admin/perfil" element={<AdminProfile />} />
@@ -36,9 +22,7 @@ export default function App() {
           <Route path="/admin/eventos" element={<AdminEvents />} />
           <Route path="/admin/reunioes" element={<AdminMeetings />} />
         </Route>
-        
-        {}
-        <Route path="*" element={<Home />} />
+        <Route path="*" element={<RootRedirect />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/routes/root-redirect.tsx
@@ -0,0 +1,12 @@
+import { Navigate } from "react-router-dom";
+import { authStorage } from "../services";
+
+export function RootRedirect() {
+  const hasToken = authStorage.getAccessToken();
+
+  if (hasToken) {
+    return <Navigate to="/admin" replace />;
+  }
+
+  return <Navigate to="/" replace />;
+}
